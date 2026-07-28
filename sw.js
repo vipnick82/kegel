c16bde5dd50a6404b707a282b138204e816588e0
vipnick82 <42087266+vipnick82@users.noreply.github.com>
2026-07-28 12:27:31 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+/* Kegel Trainer - service worker (офлайн) */
+const CACHE = 'kegel-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      if (hit) {
+        // обнови тихо на заден план
+        fetch(e.request).then(res => {
+          if (res && res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        }).catch(() => {});
+        return hit;
+      }
+      return fetch(e.request)
+        .then(res => {
+          if (res && res.ok) {
+            const copy = res.clone();
+            caches.open(CACHE).then(c => c.put(e.request, copy));
+          }
+          return res;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
